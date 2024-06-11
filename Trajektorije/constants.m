01a94504bb97750aa786b8b86395aede95b1bf90
Kristina Samardžić <kristina.samardzic@gmail.com>
2024-06-11 10:32:57 +0200

    Create constants.m

---
+++ Trajektorije/constants.m
@@ -0,0 +1,12 @@
+const.earthradius = 6325766;
+const.Hp_trop = 11000; %Geopotential pressure altitude of tropopause in meters
+const.T0 = 288.15; %Temperature at ISA MSL in kelvins
+const.P0 = 101325; %Pressure at ISA MSL in Pascals
+const.g0 = 9.80665; %Acceleration due to gravity at MSL, m/s^2
+const.a0 = 340.294; %Speed of sound at ISA 0m alt [m/s]
+const.Tisa_trop = 216.65; %Temperature at start of tropopause, Kelvins
+const.BetaT = -0.0065; %Temperature gradient K/m
+const.kappa = 1.4; %Adiabatic index of air
+const.R = 287.05287; %Specific gas constant for dry air, m^2/Ks^2
+const.ft=0.3048; %conversion of feet to meters 1 [ft] = 0.3048 [m]
+
