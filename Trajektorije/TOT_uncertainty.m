886b4c09b451dca586b2392106a0f4b7faae0955
Kristina Samardžić <kristina.samardzic@gmail.com>
2024-07-03 15:18:00 +0200

    provjera

--- Trajektorije/TOT_uncertainty.m
+++ Trajektorije/TOT_uncertainty.m
@@ -1,8 +1,8 @@
 function TOT_time_sec = TOT_uncertainty(time_to_EOBT, departure_planned_time)
     % Define the intervals (in minutes)
     
-     time_to_EOBT = 0; %in minutes
-     departure_planned_time = 30000;
+%      time_to_EOBT = 0; %in minutes
+%      departure_planned_time = 30000;
     intervals = [0, 15; 15, 30; 30, 60; 60, 90; 90, 120; 120, 180; 180, 240; 240, 360];
 
     % Define the departure times for each interval (in minutes)
